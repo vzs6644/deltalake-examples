d54e0e76e8d0f84014832c83246475b97b865b80
vzs6644 <oliver.queen853@gmail.com>
2020-11-17 07:55:48 +0530

    initial

--- com/dsm/df/limits/schema_enforcement_demo.py
+++ com/dsm/df/limits/schema_enforcement_demo.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
 
     df_path = "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/schema_enforcement"
 
-    step = "overwrite"
+    step = "append"
     if step == "overwrite":
         data = sc.parallelize([
             ("Brazil",  2011, 22.029),
